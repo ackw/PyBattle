0ff45c91257ddb26f42195de59f9d96ca00c7364
andrel <andrelchew@icloud.com>
2020-09-27 17:04:23 +0800

    logo and scenes added

--- Assets/Scenes/test/SceneChanger.cs
+++ Assets/Scenes/test/SceneChanger.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneChanger: MonoBehaviour {
-    public void Scene1() {
-        SceneManager.LoadScene("Scene1");
+
+    public void Home() {
+      SceneManager.LoadScene("Home");
     }
-    public void Scene2() {
-        SceneManager.LoadScene("Scene2");
+
+    public void Login() {
+        SceneManager.LoadScene("Login");
     }
-    public void Scene3() {
-        SceneManager.LoadScene("Scene3");
+
+    public void SelectMode() {
+        SceneManager.LoadScene("SelectMode");
     }
+
     public void Game() {
         SceneManager.LoadScene("Game");
+    }
+    
+    public void Question() {
+        SceneManager.LoadScene("Question");
+    }
+
+    public void PVP() {
+        SceneManager.LoadScene("PVP");
     }
 }
