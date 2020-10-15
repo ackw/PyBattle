26a680c4a7d85cfc6d1d9329e4046d331660cf87
Elaine <elainepakxinyi@gmail.com>
2020-10-15 22:05:58 +0800

    Fixed Character Selector

---
+++ PYBattle/Assets/GetMainChar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMainChar : MonoBehaviour
+{
+    public Sprite Char1, Char2, Char3;
+    private SpriteRenderer mySprite;
+    private readonly string SelectedChar = "SelectedCharacter";
+
+    void Awake()
+    {
+        mySprite = this.GetComponent<SpriteRenderer>();
+
+    }
+
+    void Start()
+    {
+        int getCharacter;
+        getCharacter = PlayerPrefs.GetInt(SelectedChar);
+
+        switch (getCharacter)
+        {
+            case 1:
+                mySprite.sprite = Char1;
+                break;
+            case 2:
+                mySprite.sprite = Char2;
+                break;
+            case 3:
+                mySprite.sprite = Char3;
+                break;
+            default:
+                mySprite.sprite = Char1;
+                break;
+        }
+        
+    }
+
+}
