9e0c4c15173ebcd0c5b573ff0cf09e4774d8c4d0
Fre3fall <71115114+fre3fall96@users.noreply.github.com>
2020-10-28 14:27:19 +0800

    Latest - Winson

---
+++ PYBattle/Assets/Creator Kit - RPG/Scripts/SceneAdder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneAdder : MonoBehaviour
+{
+    [SerializeField] private string newlevel;
+    public GameObject sprite;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("player"))
+        SceneManager.LoadScene(newlevel, LoadSceneMode.Additive);
+        removeSprite();
+    }
+    public void removeSprite()
+    {
+        sprite.GetComponent<SpriteRenderer>().enabled = false;
+        sprite.GetComponent<BoxCollider2D>().enabled = false;
+
+        Destroy(sprite);
+        //return;
+    }
+}
